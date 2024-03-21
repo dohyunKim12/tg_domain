a37d79b69148b8ef583c4571cbfc350b17c01497
dohyunkim <kdh951102@gmail.com>
2024-03-21 11:55:07 +0900

    fix user.dir

--- src/main/java/com/tg/url/config/TgConstants.java
+++ src/main/java/com/tg/url/config/TgConstants.java
@@ -3,11 +3,11 @@ package com.tg.url.config;
 import java.io.File;
 
 public class TgConstants {
-    public static final String USER_DIR =  System.getProperty("user.dir");
+    public static final String USER_DIR =  "/root/domainapp";
 
     public static final String DEFAULT_UPLOAD_PATH = USER_DIR + "/binary/uploads";
     public static final String DOMAIN_UPLOAD_PATH = DEFAULT_UPLOAD_PATH + "/domains";
-    public static final String CUSTOMER_UPLOAD_PATH = DEFAULT_UPLOAD_PATH + "/customers";
+    public static final String CLIENT_UPLOAD_PATH = DEFAULT_UPLOAD_PATH + "/clients";
 
     static {
         File directory = new File(DEFAULT_UPLOAD_PATH);
@@ -18,7 +18,7 @@ public class TgConstants {
         if(!directory.exists()) {
             directory.mkdirs();
         }
-        directory = new File(CUSTOMER_UPLOAD_PATH);
+        directory = new File(CLIENT_UPLOAD_PATH);
         if(!directory.exists()) {
             directory.mkdirs();
         }
